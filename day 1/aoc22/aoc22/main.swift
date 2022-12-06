f6e4b5c4b9726d330ac191fd464831f152d14d4a
Rob Stearn <robstearn@me.com>
2022-12-06 12:18:39 +0000

    Big refactor to get the top 3 part working

--- day 1/aoc22/aoc22/main.swift
+++ day 1/aoc22/aoc22/main.swift
@@ -20,30 +20,43 @@ if args.count < 2 {
     do {
         //Getting the contents of the file if the file is parsable
         let fileData = try Data(contentsOf: fileURL)
-        if let stringData = String(data: fileData, encoding: .utf8) {
-            let splitData = Array(stringData.components(separatedBy: "\n\n"))
-            //Actually doing stuff with the data
-            var maxCalories = 0
-            for elf in splitData {
-                var elfCalories = 0
-                let calorieSplit = Array(elf.components(separatedBy: "\n"))
-                for calorie in calorieSplit {
-                    elfCalories += Int(calorie) ?? 0
-                }
-                maxCalories = elfCalories > maxCalories ? elfCalories : maxCalories
-            }
-            print(maxCalories)
-        }
+        print("Total of top 3 elves: \(topThreeCaloriesTotal(fileData: fileData))")
+        print("Highest calories: \( mostCalories(fileData: fileData))")
     } catch {
-        print("Could not read data from the file at: " + path)
+        print("ERROR: Could not read data from the file at: " + path)
     }
 }
 
 
-func mostCalories(fileURL: URL) -> Int {
-    return 0
+func mostCalories(fileData: Data) -> Int {
+    var maxCalories = 0
+    if let stringData = String(data: fileData, encoding: .utf8) {
+        let splitData = Array(stringData.components(separatedBy: "\n\n"))
+        //Actually doing stuff with the data
+        for elf in splitData {
+            var elfCalories = 0
+            let calorieSplit = Array(elf.components(separatedBy: "\n"))
+            for calorie in calorieSplit {
+                elfCalories += Int(calorie) ?? 0
+            }
+            maxCalories = elfCalories > maxCalories ? elfCalories : maxCalories
+        }
+    } else {
+        print("ERROR: Could not parse data from file.")
+    }
+    return maxCalories
 }
 
-func topThreeCalories(fileURL: URL) -> [Int] {
-    return [0]
+func topThreeCaloriesTotal(fileData: Data) -> Int {
+    if let stringData = String(data: fileData, encoding: .utf8) {
+        let splitData = Array(stringData.components(separatedBy: "\n\n"))
+        //Actually doing stuff with the data
+        let parsedTotals = splitData
+            .map { Array($0.components(separatedBy: "\n")) } //Map String to String Array
+            .map { $0.map{Int($0) ?? 0 } //Map String Array to Int Array
+            .reduce(0,+) } //Total each Int Array
+            .sorted { a, b in a > b } //Order by value decreasing
+        return parsedTotals[0...2].reduce(0,+)
+    }
+    return 0
 }
